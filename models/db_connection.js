a58e7a9b5daf11e763c0b15003858af22accad77
mhaderman <haderman@seawolf.sonoma.edu>
2015-04-13 13:17:42 -0700

    fixed typo

--- models/db_connection.js
+++ models/db_connection.js
@@ -1,6 +1,6 @@
 exports.config = {
         user: 'your_mysql_username',
         password: 'your_mysql_password',
-        host: 'they_mysql_server',
+        host: 'the_mysql_server',
         database: 'your_mysql_database'
 };
