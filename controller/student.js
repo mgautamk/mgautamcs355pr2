5fb3013f483b0283aa5526f4cd506fa43a4bf8c4
mhaderman <haderman@seawolf.sonoma.edu>
2015-04-29 15:34:21 -0700

    added an ajax drop down menu example that makes a request on a selection change.

--- controller/student.js
+++ controller/student.js
@@ -29,6 +29,23 @@ router.get('/', function (req, res) {
     }
 });
 
+
+/* View a single students information */
+router.post('/', function (req, res) {
+    if(req.body.studentid == null) {
+        res.send("The studentid parameter was not provided")
+    }
+    else {
+        db.GetByID(req.body.studentid, function (err, result) {
+                if (err) throw err;
+
+                // Send result to the template along with the original student id in case there were no results
+                res.render('displayStudentInfoSnippet.ejs', {rs: result, studentid: req.body.studentid});
+            }
+        );
+    }
+});
+
 // Create Student Form
 router.get('/create', function(req, res){
     res.render('createStudentForm.ejs', {action: '/student/create'});
@@ -61,6 +78,15 @@ router.get('/dropdown', function (req, res) {
     db.GetAllView(function (err, result) {
             if (err) throw err;
             res.render('displayStudentDropDown.ejs', {rs: result});
+        }
+    );
+});
+
+/* View all users in a drop down menu */
+router.get('/dropdown_ajax', function (req, res) {
+    db.GetAllView(function (err, result) {
+            if (err) throw err;
+            res.render('displayStudentDropDownAJAX.ejs', {rs: result});
         }
     );
 });
---
+++ public/select_student_ajax.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#studentid').change( function(){
+
+        if($('#studentid').val() == "") {
+            // the user selected the blank option, so hide the div and return
+            $('#output').hide();
+            return;
+        }
+
+        var payload = {
+            studentid: $('#studentid').val()
+        };
+
+        console.log(payload);
+
+        $.ajax({
+            url: $("#ajax_form_example").attr("action"),
+            type: "POST",
+            contentType: "application/json",
+            processData: false,
+            data: JSON.stringify(payload),
+            complete: function(data) {
+                console.log(data.responseText);
+                $('#output').html(data.responseText);
+                $('#output').show();
+            }
+        });
+    });
+});
